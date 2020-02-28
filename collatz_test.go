3393b40ffafdf00699220af42e02dbeeabdc1d0e
Michel Casabianca <casa@sweetohm.net>
2020-02-28 13:38:09 +0100

    Added unit tests

--- collatz_test.go
+++ collatz_test.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
-	if false {
-		t.Errorf("There was an error in the fabric of space-time!")
+	expected := []int{7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1}
+	actual := collatz(7)
+	if !reflect.DeepEqual(expected, *actual) {
+		t.Errorf("Bad collatz series: %v", actual)
+	}
+	if maxList(actual) != 52 {
+		t.Errorf("Bad maximum: %d", maxList(actual))
+	}
+	if listToString(actual) != "7 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1" {
+		t.Errorf("Bad string list: %s", listToString(actual))
 	}
 }
